663f39ee1770edb3b4787ddace1f422f0a7b9c9c
grbcp5 <grbcp5@mst.edu>
2017-06-18 20:35:12 -0500

    Add cookie parser and express session.

--- server.js
+++ server.js
@@ -4,10 +4,26 @@ var app = express();
 var morgan = require( 'morgan' );
 app.use( morgan('dev') );
 
+var cookieParser = require( 'cookie-parser' );
+app.use( cookieParser() );
+
+var expressSession = require( 'express-session' );
+app.use( expressSession( {
+  
+  secret: 'anyStringOfText',
+  saveUninitialized: true,
+  resave: true
+
+} ) );
+
 var port = process.env.PORT || 8080;
 
 app.use( '/', function( req, res ) {
   
+  console.log( req.cookies );
+  console.log( "=========" );
+  console.log( req.session );
+
   res.send( "Hello, world!" );
 
 } );
